4dcbfed69d8778677e4c993d90f6d48cfed8d059
zhiyuan_zhu <zzyjsjcom@126.com>
2016-09-20 12:55:33 +0800

    edit code

--- hello.s
+++ hello.s
@@ -1,9 +1,4 @@
-	.cpu generic+fp+simd
-	.text
-.Ltext0:
-	.cfi_sections	.debug_frame
 	.align	2
-
 	.global	add
 	.type	add, %function
 add:
@@ -24,8 +19,3 @@ add64:
 	add	x0, x0, x1
 	ret
 
-	.section	.rodata.str1.8,"aMS",%progbits,1
-	.align	3
-.LC1:
-	.string	"[zzy test] hello1 arm64\n"
-	.text
